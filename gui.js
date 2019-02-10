232040c5a0c2027ffd06b28b1d6d3f707b7bde61
Umur Can Gürelli <umurcg@gmail.com>
2019-02-11 01:18:25 +0300

    refactored

--- gui.js
+++ gui.js
@@ -29,114 +29,78 @@ function buildGUI(){
  	return new dat.GUI();
 }
 
-function lightController(gui){
+function lightController(gui, light){
+
+	var parameters={
+		lightIntensity:light.intensity,
+		lightColor:light.color,
+		sunPosition:{x:light.position.x,y:light.position.y,z:light.position.z}						
+	}
+
+
+	function updateRotation(){
+		var x=parameters.sunPosition.x;
+		var y=parameters.sunPosition.y;
+		var z=parameters.sunPosition.z;				
+		light.position.set(x, y , z);
+		
+	}
+
+	var lightController= gui.addFolder("Light Controller");
+	var lightRotController=lightController.addFolder("Sun Position");
+	lightRotController.add(parameters.sunPosition,"x").onChange(updateRotation);
+	lightRotController.add(parameters.sunPosition,"y").onChange(updateRotation);
+	lightRotController.add(parameters.sunPosition,"z").onChange(updateRotation);
+
+	lightController.add(parameters,"lightIntensity").onChange(function(){								
+		light.intensity=parameters.lightIntensity;
+		// render();
+	});
+
+	lightController.addThreeColor(parameters,"lightColor");
+
 
 }
 
 
-function sceneController(gui){
+function sceneController(gui, scene){
 
+	var parameters={
+		sceneColor:scene.background		
+	}
+
+	var sceneController=gui.addFolder("Scene Controller");
+	sceneController.addThreeColor(parameters,"sceneColor");			
 }
 
 function textureController(gui){
-
-}
-
-
-function modelImporter(gui){
+	var textureFolder=gui.addFolder("Texture Controller");
 
 
+	function changeObjectTexture(object){
 
-    var importFBXButton = { ImportModel:function(){ 
-
-    	//Imports fbx from local of user
-    	importFBX(function(object){
-
-    		//Remove previos object if exists
-    		if(objectGroup!==undefined)
-    			scene.remove(objectGroup);
-
-    		objectGroup=object;
-    		scene.add(objectGroup)
-    		fixCameraToObject();
-    		updateTextureController();
-    		controls.update();
-    		render();
-    		
-          		            	         
-    	});
-
-    }};
-	gui.add(importFBXButton,'ImportModel');
+		importTexture(function(texture){			
+			object.material.map=texture;
+			object.material.needsUpdate = true;						
+		})
+    }
 
 
-}
-
-  //UI
-           
-
-        
-			
-
-
-			var parameters={
-				lightIntensity:light.intensity,
-				lightColor:light.color,
-				sunPosition:{x:light.position.x,y:light.position.y,z:light.position.z},
-				sceneColor:scene.background
-				
-			}
-
-			var sceneController=gui.addFolder("Scene Controller");
-
-
-			sceneController.add(parameters,"lightIntensity").onChange(function(){								
-				light.intensity=parameters.lightIntensity;
-				render();
-			});
-			sceneController.addThreeColor(parameters,"lightColor");
-
-			function updateRotation(){
-				var x=parameters.sunPosition.x;
-				var y=parameters.sunPosition.y;
-				var z=parameters.sunPosition.z;				
-				light.position.set(x, y , z);
-				render();				
-			}
-
-
-			var lightRotController=sceneController.addFolder("Sun Position");
-			lightRotController.add(parameters.sunPosition,"x").onChange(updateRotation);
-			lightRotController.add(parameters.sunPosition,"y").onChange(updateRotation);
-			lightRotController.add(parameters.sunPosition,"z").onChange(updateRotation);
-			
-			sceneController.addThreeColor(parameters,"sceneColor");			
-
-			var textureFolder=gui.addFolder("Texture Controller");
-
-			function changeObjectTexture(object){
-
-				importTexture(function(texture){
-					
-					object.material.map=texture;
-					object.material.needsUpdate = true;
-					render();
-					console.log("updated material");
-				})
-
-            }
-
-
-			function updateTextureController(){
-				textureFolder.removeFolder();
-				var objects=objectGroup.children;
-
-				for(let o in objects){
-					let buttonFunction={};
-					buttonFunction[objects[o].name]=function(){
-						changeObjectTexture(objects[o])
-					}
-					var changeTextButton=textureFolder.add(buttonFunction,objects[o].name);
-				}
-			}
+	this.clear=textureFolder.removeFolder;
+	
+	this.updateObjects=function(objects){
 		
+		textureFolder.removeFolder();
+		for(let o in objects){
+				let buttonFunction={};
+				buttonFunction[objects[o].name]=function(){
+				changeObjectTexture(objects[o])
+			}
+			var changeTextButton=textureFolder.add(buttonFunction,objects[o].name);
+		}
+	}
+	
+}
+
+
+
